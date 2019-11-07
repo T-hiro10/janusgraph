5b813c8bf14222a27cafb2f681cedc237709905a
T-hiro10 <h-tanaka@ist.osaka-u.ac.jp>
2019-11-07 20:03:28 +0900

    tmp

--- janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/ManagementLogger.java
+++ janusgraph-core/src/main/java/org/janusgraph/graphdb/database/management/ManagementLogger.java
@@ -204,14 +204,22 @@ public class ManagementLogger implements MessageReader {
 try (FileOutputStream fileOutputStream = new FileOutputStream(Paths.get("/home/travis/stream_method_stacktrace.txt").toFile(), true);
 	OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, Charset.forName("UTF-8"));
 	BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter)) {
+	String projectNameString = "janusgraph";
 	final StackTraceElement[] stackTrace = new RuntimeException().getStackTrace();
 	bufferedWriter.newLine();
+	boolean isFirstStackTrace = true;
+	String lastStackTrace = "";
 	for (final StackTraceElement stackTraceElement : stackTrace) {
-		if(stackTraceElement.toString().contains("janusgraph")) {
+		if(isFirstStackTrace && stackTraceElement.toString().contains(projectNameString)) {
 			bufferedWriter.append(stackTraceElement.toString());
 			bufferedWriter.newLine();
+			isFirstStackTrace = false;
+		} else if(!(isFirstStackTrace) && stackTraceElement.toString().contains(projectNameString)) {
+			lastStackTrace = stackTraceElement.toString();
 		}
 	}
+	bufferedWriter.append(lastStackTrace);
+	bufferedWriter.newLine();
 } catch (Exception e) {
 	e.printStackTrace();
 }
